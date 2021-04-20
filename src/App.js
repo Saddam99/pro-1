a46fc405a4751e88d27f0c4042a2bdfdb8dcef34
Saddam <visinwork@gmail.com>
2021-04-21 00:45:40 +0300

    pro-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((respons) => {
+          return respons.json()
+        })
+        .then((arr) => {
+          setData(arr);
+          setLoading(false);
+        })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        список дел
+      </div>
+      {loading ? (
+          <div>
+            идет загрузка
+          </div>
+      ) : data.map((item) => {
+        return (
+            <div className="todo">
+             {item.id}
+             <div>
+                 {item.title}
+             </div>
+            </div>
+        )
+      }) }
+
     </div>
   );
 }
